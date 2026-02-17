4be047c26c818b499ef69b23dc37c91ce8f79e2a
Akshat2711 <akshatsrivastava206@gmail.com>
2026-02-17 22:34:41 +0530

    added backend integration for lecture

---
+++ microservices/video_lecture_agent/tools/doubt_agent.py
@@ -0,0 +1,35 @@
+import os
+import json
+from google import genai
+from dotenv import load_dotenv
+from tools.prompt import PROMPT_TEMPLATE_DOUBT_CLEAR
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+async def solve_doubt(text: str) -> dict:
+    prompt = PROMPT_TEMPLATE_DOUBT_CLEAR.format(input_text=text)
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash-lite",
+            contents=prompt,
+            config={"response_mime_type": "application/json"},
+        )
+
+        print(response)
+
+        parsed = json.loads(response.text.strip())
+
+        return {
+            "resp": parsed.get("doubt_clear", ""),
+            "status":200
+        }
+
+    except Exception as e:
+        return {
+            "doubt_clear": "",
+            "status":500
+        }
--- microservices/video_lecture_agent/tools/prompt.py
+++ microservices/video_lecture_agent/tools/prompt.py
@@ -23,3 +23,33 @@ STRICT JSON FORMAT ONLY:
 Text:
 {input_text}
 """
+
+
+
+#prompt to clear doubt of user 
+PROMPT_TEMPLATE_DOUBT_CLEAR = """
+This text contains a student's doubt or concept needing clarification.
+
+Your role:
+- Act as a knowledgeable teacher.
+- Clearly explain the concept to remove the student's doubt.
+- Use simple, precise language.
+- Include examples where helpful.
+- Focus on understanding, not summarizing.
+
+Additionally:
+If an image would help understanding,
+set image_needed="yes" and describe useful images.
+Otherwise set image_needed="no" and keep image_of empty.
+
+STRICT JSON FORMAT ONLY:
+
+{
+  "doubt_clear": "clear teacher-style explanation resolving the doubt",
+}
+
+Student doubt text:
+{input_text}
+"""
+
+
